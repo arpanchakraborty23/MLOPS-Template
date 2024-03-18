98489914784535bbcfc72d683a1f7c3177b8a54e
ARPAN CHAKRABORTY <127507112+arpanchakraborty23@users.noreply.github.com>
2024-03-18 11:42:43 +0530

    Update setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup,find_packages
+from typing import List
+
+Hypen_dot_e='-e .'
+
+def get_requirements(file_path:str)->List[str]:
+    requirements=[]
+    with open(file_path,'r') as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("/n","") for req in requirements]
+
+        if Hypen_dot_e in requirements:
+            requirements.remove(Hypen_dot_e)
+    return requirements
+
+__version__='0.0.1'
+Repo_name= ''
+package_name=''
+Author='Arpanchakraborty23'
+Author_email='arpanchakraborty500@gmail.com'
+
+setup(
+    name=package_name,
+    version=__version__,
+    author=Author,
+    author_email=Author_email,
+  
+    
+    long_description_content='text/markdown',
+    url=f"https//:github.com/{Author}/{Repo_name}",
+    packages=find_packages(),
+    install_requries=get_requirements('requirements_dev.txt')
+)
